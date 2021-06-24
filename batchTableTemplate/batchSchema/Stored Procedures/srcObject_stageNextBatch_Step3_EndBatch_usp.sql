1437b9c3e35941d0dce68aa81d3e2e7c76b87665
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-06-24 05:07:37 -0400

    draft of step 3 related to #5

--- batchTableTemplate/batchSchema/Stored Procedures/srcObject_stageNextBatch_Step3_EndBatch_usp.sql
+++ batchTableTemplate/batchSchema/Stored Procedures/srcObject_stageNextBatch_Step3_EndBatch_usp.sql
@@ -1,6 +1,24 @@
 ﻿CREATE PROCEDURE [batchSchema].[srcObject_stageNextBatch_Step3_EndBatch_usp]
-	@param1 int = 0,
-	@param2 int
 AS
-	SELECT @param1, @param2
-RETURN 0
+
+	DECLARE @BatchId INT = NULL;
+	DECLARE @CountRows INT = NULL;
+	DECLARE @MaximumValue_Inclusive BINARY(8) = NULL;
+
+	SELECT
+	 @BatchId = MAX([BatchID])
+	,@MaximumValue_Inclusive = MAX([IncrementalColumnName])
+	,@CountRows = COUNT(1)
+	FROM [stagedSchema].[srcObject_stg]
+	;
+
+	/*NOTE: We set the max to the actual max, overwriting any max provided at batch start
+	to ensure that future batches include all values.*/
+
+	UPDATE [batchSchema].[srcObject_batches]
+	SET BatchEnd = SYSUTCDATETIME()
+	,[Batch_Row_Count] = @CountRows
+	,[MaximumValue_IncrementalColumnName] = @MaximumValue_Inclusive
+	,[BatchSuccessful] = 1
+	WHERE [BatchId] = @BatchId
+	;
